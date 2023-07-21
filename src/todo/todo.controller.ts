6fca4e045c496f30ed96027d326b8f871c67d56b
ChristianARP <94751604+Chrisburg22@users.noreply.github.com>
2023-07-20 19:41:43 -0600

    feat: Create the todo resource

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,37 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { TodoService } from './todo.service';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+
+@Controller('todo')
+export class TodoController {
+  //Injeccion de dependencia
+  constructor(private readonly todoService: TodoService) {}
+
+  //Como podemos ver en cada funcion podemos ver una misma estructura la cual es: 
+  //Un tipo de solicitud y retornar algun metodo de la instancia de los serivios
+  @Post()
+  create(@Body() createTodoDto: CreateTodoDto) {
+    return this.todoService.create(createTodoDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.todoService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.todoService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+    return this.todoService.update(+id, updateTodoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.todoService.remove(+id);
+  }
+}
